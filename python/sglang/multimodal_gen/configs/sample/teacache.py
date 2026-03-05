23c7ee4e8c190c6d3da56e9bff1c3fce0785ea5b
Eitan Turok <eitan.turok@gmail.com>
2026-03-05 16:32:00 +0000

    dont need params here

--- python/sglang/multimodal_gen/configs/sample/teacache.py
+++ python/sglang/multimodal_gen/configs/sample/teacache.py
@@ -23,26 +23,8 @@ class WanTeaCacheParams(CacheParams):
     skip_start_step: int = 5
     skip_end_step: int = 0
     use_ret_steps: bool = True
-    # from original teacache paper https://github.com/ali-vilab/TeaCache/blob/7c10efc4702c6b619f47805f7abe4a7a08085aa0/TeaCache4Wan2.1/teacache_generate.py#L883
-    ret_steps_coeffs: list[float] = field(
-        default_factory=lambda: [
-            -5.21862437e04,
-            9.23041404e03,
-            -5.28275948e02,
-            1.36987616e01,
-            -4.99875664e-02,
-        ]
-    )
-    # from original teacache paper https://github.com/ali-vilab/TeaCache/blob/7c10efc4702c6b619f47805f7abe4a7a08085aa0/TeaCache4Wan2.1/teacache_generate.py#L890
-    non_ret_steps_coeffs: list[float] = field(
-        default_factory=lambda: [
-            2.39676752e03,
-            -1.31110545e03,
-            2.01331979e02,
-            -8.29855975e00,
-            1.37887774e-01,
-        ]
-    )
+    ret_steps_coeffs: list[float] = field(default_factory=list)
+    non_ret_steps_coeffs: list[float] = field(default_factory=list)
 
     @property
     def coefficients(self) -> list[float]:
